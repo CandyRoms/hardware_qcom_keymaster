e3ea08db1123d4ff4efb9d53a31f9d96da2f49b7
Craig Tatlor <ctatlor97@gmail.com>
2018-05-21 17:18:33 +0100

    keymaster: Build with BOARD_VNDK_VERSION
    Adds libhardware as a lib and replaces nativebase's UniquePtr as
    nativebase isnt a vndk library.
    
    Change-Id: I32691b7543a35ece110d25fe907c181218801755

--- Android.mk
+++ Android.mk
@@ -37,6 +37,7 @@ LOCAL_SHARED_LIBRARIES := \
         liblog \
         libc \
         libdl \
+        libhardware \
         libcutils
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
